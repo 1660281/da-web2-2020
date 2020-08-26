fa6d1d513741e98cdc30d35ca23ab7841ee7bb80
binh <binhle.101299@gmail.com>
2020-08-26 10:13:08 +0700

    file regist lan ba

--- routes/regist.js
+++ routes/regist.js
@@ -1,3 +1,33 @@
-﻿router.get('/regist', function getRegist(req, res, next) {
-    res.render(`${regist}regist`);
-});
+﻿router.post('/regist',upload.single('crePic'), asyncHandler(async function (req, res, next) {
+    try {
+        var user = await User.createUser({
+            email: req.body.email,
+            password: req.body.password,
+            fullName: req.body.fullName,
+            creID: req.body.creID,
+            creType: req.body.creType,
+            creGrantDate: req.body.creGrantDate,
+            crePic: req.file.path
+        });
+        console.log('post regist ... ');
+        console.log(user);
+
+        // uploadFile(req, res, (error) => {
+        //     // Nếu có lỗi thì trả về lỗi cho client.
+        //     // Ví dụ như upload một file không phải file ảnh theo như cấu hình của mình bên trên
+        //     if (error) {
+        //         return res.send(`Error when trying to upload: ${error}`);
+        //     }
+        //     // Không có lỗi thì lại render cái file ảnh về cho client.
+        //     // Đồng thời file đã được lưu vào thư mục uploads
+        // });
+        res.redirect('/login');
+    } catch (error) {
+        console.log(error);
+        res.render(`${regist}regist`);
+    }
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+}));
+
+module.exports = router;
