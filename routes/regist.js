bfb9faffe83bb60f6e9b35b68b48486f9269ffd4
binh <binhle.101299@gmail.com>
2020-08-26 10:11:42 +0700

    file regist lan hai

--- routes/regist.js
+++ routes/regist.js
@@ -1,32 +1,3 @@
-﻿
-const { Router } = require('express');
-const users = require('../services/users');
-const asyncHandler = require('express-async-handler');
-const router = new Router();
-const multer = require('multer');
-const User = require('../services/users');
-const upload = multer({ dest: 'uploads/' });
-const regist = './regist/';
-let diskStorage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        // Định nghĩa nơi file upload sẽ được lưu lại
-        callback(null, "uploads");
-    },
-    filename: (req, file, callback) => {
-        // ở đây các bạn có thể làm bất kỳ điều gì với cái file nhé.
-        // Mình ví dụ chỉ cho phép tải lên các loại ảnh png & jpg
-        let math = ["image/png", "image/jpeg"];
-        if (math.indexOf(file.mimetype) === -1) {
-            let errorMess = `The file <strong>${file.originalname}</strong> is invalid. Only allowed to upload image jpeg or png.`;
-            return callback(errorMess, null);
-        }
-        // Tên của file thì mình nối thêm một cái nhãn thời gian để đảm bảo không bị trùng.
-        let filename = `${Date.now()}-koi-${file.originalname}`;
-        callback(null, filename);
-    }
+﻿router.get('/regist', function getRegist(req, res, next) {
+    res.render(`${regist}regist`);
 });
-// Khởi tạo middleware uploadFile với cấu hình như ở trên,
-// Bên trong hàm .single() truyền vào name của thẻ input, ở đây là "file"
-let uploadFile = multer({ storage: diskStorage }).single("file");
-
-
