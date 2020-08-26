9b876f4f022ef20b27bb9729752364d13b3cfa30
nhatduy1998 <50981600+nhatduy1998@users.noreply.github.com>
2020-08-26 18:26:18 +0700

    them trang login, trang regist

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,4 @@
+
 const { Router } = require('express');
 const funcs = require('../services/users');
 const asyncHandler = require('express-async-handler');
@@ -10,3 +11,29 @@ router.get('/login', function getLogin(req, res, next) {
         res.redirect('/');
 });
 
+
+router.post('/login', asyncHandler(async function postLogin(req, res, next) {
+    console.log('post login ...');
+    user = await funcs.findUserByEmail(req.body.email);
+    if (user!= null && funcs.verifyPassword(req.body.password, user.password)) {
+        req.session.user = user;
+        if (user.role == 0)
+            res.redirect('/');
+        else
+            res.redirect('/emp');
+    }
+    else {
+        messageResult = 'Email hoặc password không đúng.';
+        res.render('login/login', { messageResult });
+    }
+}));
+
+
+router.get('/logout', function logout(req, res, next) {
+    if (req.session.user != null)
+        console.log('logout user: ' + req.session.user.id);
+    req.session.user = null;
+    res.redirect('/login');
+});
+
+module.exports = router;
